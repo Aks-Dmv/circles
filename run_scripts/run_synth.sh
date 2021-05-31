52bdf06e8c6fcb03188990d2528b0143320ca44f
Aks-Dmv <akshaydharmavaram@gmail.com>
2021-05-31 17:51:44 -0400

    Revert "best baseline"
    This reverts commit 1e56387572625ab0006683ca8a69f6819e9e4aa1.

--- run_scripts/run_synth.sh
+++ run_scripts/run_synth.sh
@@ -30,7 +30,7 @@ do
     HIDDEN_ARGS="--rnn_hidden 32"
     PRIOR_ARGS="--use_learned_prior --prior_num_layers 3 --prior_hidden_size 32"
     MODEL_ARGS="--model_type dnri --graph_type dynamic --skip_first --num_edge_types 2 $ENCODER_ARGS $DECODER_ARGS $HIDDEN_ARGS $PRIOR_ARGS --seed ${SEED}"
-    TRAINING_ARGS='--add_uniform_prior --no_edge_prior 0.2 --batch_size 256 --lr 1e-4 --use_adam --num_epochs 1500 --lr_decay_factor 1.0 --lr_decay_steps 100 --normalize_kl --normalize_nll --tune_on_nll --val_teacher_forcing --teacher_forcing_steps -1 --train_hard_sample'
+    TRAINING_ARGS='--add_uniform_prior --no_edge_prior 0.2 --batch_size 256 --lr 5e-4 --use_adam --num_epochs 1500 --lr_decay_factor 1.0 --lr_decay_steps 100 --normalize_kl --normalize_nll --tune_on_nll --val_teacher_forcing --teacher_forcing_steps -1 --train_hard_sample'
     mkdir -p $WORKING_DIR
     CUDA_VISIBLE_DEVICES=$GPU python -u dnri/experiments/small_synth_experiment.py --gpu --mode train --data_path $DATA_PATH --working_dir $WORKING_DIR $MODEL_ARGS $TRAINING_ARGS |& tee "${WORKING_DIR}results.txt"
     CUDA_VISIBLE_DEVICES=$GPU python -u dnri/experiments/small_synth_experiment.py --gpu --load_best_model --mode eval --data_path $DATA_PATH --working_dir $WORKING_DIR $MODEL_ARGS $TRAINING_ARGS |& tee "${WORKING_DIR}eval_results.txt"
